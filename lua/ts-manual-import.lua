0c630ffe18d68ae007073163cfbaa9008d9257d1
dorage <baloonflower554@gmail.com>
2024-07-14 22:44:50 +0900

    remove: redundant code

--- lua/ts-manual-import.lua
+++ lua/ts-manual-import.lua
@@ -16,9 +16,7 @@ M.luasnip_callback = function(imports)
   return {
     [-1] = {
       [require("luasnip.util.events").enter] = function()
-        vim.schedule(function()
-          M.import(imports)
-        end)
+        M.import(imports)
       end,
     },
   }
