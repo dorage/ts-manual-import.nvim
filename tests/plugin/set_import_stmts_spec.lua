d6007d0668e0eba311684617344a9c73a5f0f1cc
dorage <baloonflower554@gmail.com>
2024-07-14 20:39:26 +0900

    test: add test cases

---
+++ tests/plugin/set_import_stmts_spec.lua
@@ -0,0 +1,8 @@
+local import = require("ts-manual-import.module")
+
+describe("add import statements", function()
+  it("in the blank file", function() end)
+  it("in the file, that have no import statements", function() end)
+  it("in the file, that have import statements, but sources are not overlapped", function() end)
+  it("in the file, that have import statements, but sources are overlapped", function() end)
+end)
